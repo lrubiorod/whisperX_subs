86cdd7f0658a8988804664ede2a5008db0ca3998
Luis Rubio <l.rubiorod@gmail.com>
2023-09-21 17:38:33 +0200

    feat: include several languages translations

--- whisperx_subs/whisperx_subs.py
+++ whisperx_subs/whisperx_subs.py
@@ -52,80 +52,86 @@ def split_text_into_equal_segments(segment_text, n_segments):
 
     return segments
 
-def convert_json_to_srt(json_str, max_char, translator_model=None):
+def convert_json_to_srt(json_str, max_char, langs=[], model_name='Helsinki-NLP/opus-mt'):
     """
     Convert a given JSON string to SRT format.
     
     Parameters:
     - json_str (str): The JSON string containing segments and words data.
     - max_char (int): The maximum number of characters for each SRT segment.
-    - translate (str): Language pair for translation, e.g., 'en-es'. Default is None.
+    - langs (list): List of target languages for translation.
+    - model_name (str): Base model name for translation.
 
     Returns:
-    - str: The formatted SRT string.
+    - list: A list containing the original SRT and its translations.
     """
-
-    # Parse the input JSON string.
-    data = json.loads(json_str)
     
-    srt_output = ""
+    # Load the JSON content
+    data = json.loads(json_str)
+    source_lang = data["language"]
+
+    # Create translator models for each target language
+    translators = {}
+    for lang in langs:
+        translators[lang] = pipeline('translation', model=f'{model_name}-{source_lang}-{lang}')
+
+    # Initialize the srt outputs for each language (source + target languages)
+    srt_outputs = {lang: "" for lang in [source_lang] + langs}
+
     index = 1 
     start_word = 0
     end_word = 0
-    source_lang = data["language"]
-
-    # If translation is required, initialize the translation pipeline
-    translator = None
-    if translator_model:
-        translator = pipeline('translation', model=translator_model)
-
-    # Iterate over each segment in the data.
+    
+    # Iterate over each segment to create the srt content
     for segment in tqdm(data["segments"], desc="Converting segments", ncols=100):
         words = segment['words']
         segment_text = segment['text']
         len_text = len(segment_text)
 
-        # Determine the number of segments needed based on the max_char limit.
+        # Calculate number of srt segments needed based on max_char limit
         n_segments = (len_text // max_char) + 1
         len_segment = len_text // n_segments
-        text = ""
+        texts = {lang: "" for lang in srt_outputs.keys()}
 
-        # Translate the segment_text if required
-        translated_segments = None
-        if translator:
-            translated_text = translator(segment_text, max_length=10000)[0]['translation_text']
-            translated_segments = split_text_into_equal_segments(translated_text, n_segments)
+        # Translate and split translations into segments
+        translated_texts_segments = {}
+        for lang in langs:
+            translated_text = translators[lang](segment_text, max_length=10000)[0]['translation_text']
+            translated_texts_segments[lang] = split_text_into_equal_segments(translated_text, n_segments)
 
-        # Get the start time of the segment or use the end time of the last word if not available.
         start_time = segment.get('start', end_word)
-
-        i_translated_segment = 0
-        # Iterate over each word in the segment.
+        i_translated_segment = {lang: 0 for lang in langs}
+        
+        # Process each word in the segment
         for word_index, word in enumerate(words):
             start_word = word.get('start', end_word)
             end_word = word.get('end', start_word)
 
-            # If adding the current word exceeds the character limit or it's the last word, finalize the segment.
-            if len(text + word['word']) > len_segment or word_index == len(words) - 1:
-                text += word['word']
-                end_time = end_word
-                
-                if translated_segments:
-                    srt_output += format_segment_to_srt(index, start_time, end_time, translated_segments[i_translated_segment])
-                    i_translated_segment += 1
-                else:
-                    srt_output += format_segment_to_srt(index, start_time, end_time, text)
-                
+            # Check if the current srt segment is full or it's the last word
+            if len(texts[source_lang] + word['word']) > len_segment or word_index == len(words) - 1:
+                for lang, text in texts.items():
+                    end_time = end_word
+
+                    # If it's the source language, append the word, else append the translated segment
+                    if lang == source_lang:
+                        text += word['word']
+                        srt_outputs[lang] += format_segment_to_srt(index, start_time, end_time, text)
+                    else:
+                        translated_segment = translated_texts_segments[lang][i_translated_segment[lang]]
+                        srt_outputs[lang] += format_segment_to_srt(index, start_time, end_time, translated_segment)
+                        i_translated_segment[lang] += 1
+                        
                 index += 1
-                text = ""
+                texts = {lang: "" for lang in srt_outputs.keys()}
             else:
-                if len(text) == 0:
+                if len(texts[source_lang]) == 0:
                     start_time = start_word
                     
+                # Handle cases for languages like Japanese and Chinese differently
                 if source_lang == "ja" or source_lang == "zh":
-                    text += word['word']
+                    texts[source_lang] += word['word']
                 else:
-                    text += word['word'] + " "
+                    texts[source_lang] += word['word'] + " "
 
-    return srt_output
+    return list(srt_outputs.values())
 
